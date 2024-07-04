28025f39ccdf6ac4db2de1922162cdd50def5095
seanolero <seanolero@outlook.com>
2024-07-04 11:31:53 +0300

    experiences added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Banner from "@/components/home/banner";
 import Experiences from "@/components/home/experiences";
+import Solution from "@/components/home/solution";
 
 export default function Home() {
   return (
       <div className={'pt-16 bg-white text-dark'}>
         <Banner/>
           <Experiences />
+          <Solution/>
       </div>
   );
 }
