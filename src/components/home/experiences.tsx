33a56afebe09b50a330b3303033505f065d17c31
seanolero <seanolero@outlook.com>
2024-07-04 11:31:53 +0300

    experiences added

--- src/components/home/experiences.tsx
+++ src/components/home/experiences.tsx
@@ -6,48 +6,57 @@ import {BiChevronDown} from "react-icons/bi";
 import {useState} from "react";
 import experiences from "@/data/experiencesData";
 
-const experiencesTypes = ['All experiences', 'Photography', 'Adventure', 'Celebration', 'Family', 'Culinary','Speciality', 'Luxury', 'Voyages']
+const experiencesTypes = ['All experiences', 'Photography', 'Adventure', 'Celebration', 'Family', 'Culinary', 'Speciality', 'Luxury', 'Voyages']
 
 export default function Experiences() {
     const [currentIndex, setCurrentIndex] = useState(0);
-    return <section className={'px-16 my-24 bg-white'}>
-        <div className={'grid grid-cols-3 md:gap-16'}>
+    return <section className={'px-24 my-24 bg-white'}>
+        <div className={'grid grid-cols-3 md:gap-24'}>
             <div>
                 <h2 className={'text-6xl font-bold'}>Africa</h2>
                 <p>
 
                 </p>
             </div>
-           <ExperienceCard experience={experiences[0]}/>
+            <ExperienceCard experience={experiences[0]}/>
             <div className={'text-end flex flex-col gap-4'}>
                 <div className={'rounded-full border border-dark py-2 px-3 w-max flex ms-auto'}>Africa <BiChevronDown
                     size={24}/></div>
                 <h2 className={'text-3xl pb-2'}>Select experience type</h2>
                 <div className={'flex flex-row gap-4 ms-auto w-2/3 justify-end flex-wrap'}>
-                    {experiencesTypes.map((experience,index) => <Pill key={index} text={experience} active={index === currentIndex} onClick={() => setCurrentIndex(index)}/>)}
+                    {experiencesTypes.map((experience, index) => <Pill key={index} text={experience}
+                                                                       active={index === currentIndex}
+                                                                       onClick={() => setCurrentIndex(index)}/>)}
                 </div>
             </div>
-            {experiences.slice(1).map((experience, index) => <ExperienceCard experience={experience} key={index}/>)}
+            {experiences.slice(1, 6).map((experience, index) => <ExperienceCard experience={experience} key={index}/>)}
+            <div className={'flex flex-col justify-between px-4'}>
+                <p>Africa has an immense amount to offer as a travel destination. Discover your own africa, the most
+                    important question is, where is heart set to visiting first? </p>
+                <Pill text={'View More'}/>
+            </div>
         </div>
     </section>
 }
 
 
-function ExperienceCard({experience}: {experience: {
-    id: number,
+function ExperienceCard({experience}: {
+    experience: {
+        id: number,
         location: string,
         image: string,
         favorite: boolean,
         activity: string,
-    }}) {
+    }
+}) {
     return <div className={'rounded-lg p-8 bg-cover flex flex-col justify-between text-white bg-blend-darken'} style={{
         backgroundImage: `url(${experience.image})`,
-        aspectRatio: '0.8'
+        aspectRatio: '0.87'
     }}>
         <div className={'flex justify-between '}>
             <div className={'rounded-full bg-white text-dark py-2 px-3'}>Adventure</div>
             <div className={'rounded-full w-11 h-11 shrink-0 grow-0 aspect-square bg-white bg-opacity-50 py-2'}>
-                <HeartIcon className={`mx-auto h-7 ${experience.favorite? 'fill-primary' : ''}`}/></div>
+                <HeartIcon className={`mx-auto h-7 ${experience.favorite ? 'fill-primary' : ''}`}/></div>
         </div>
         <div className={'backdrop-blur-sm bg-opacity-20 bg-dark px-3 py-2 rounded-xl'}>
             <h2 className={'text-3xl pb-4'}>{experience.activity}</h2>
@@ -56,9 +65,9 @@ function ExperienceCard({experience}: {experience: {
     </div>
 }
 
-function Pill(this: any, {text, active, onClick}: { text: string, active: boolean, onClick?: () => void }) {
+function Pill(this: any, {text, active = false, onClick}: { text: string, active?: boolean, onClick?: () => void }) {
 
     return <div
-        className={`${active ? 'bg-dark text-white' : 'bg-white text-dark'} rounded-full py-2 px-3 text-nowrap border border-gray-500 active:bg-dark active:text-white hover:bg-dark hover:text-white`}
+        className={`${active ? 'bg-dark text-white' : 'bg-white text-dark'} rounded-full py-2 px-3 w-max text-nowrap border border-gray-500 active:bg-dark active:text-white hover:bg-dark hover:text-white`}
         onClick={onClick?.bind(this)}>{text}</div>
 }
---
+++ src/components/home/solution.tsx
@@ -0,0 +1,61 @@
+import {faker} from "@faker-js/faker";
+import {AiFillCiCircle} from "react-icons/ai";
+
+
+export default function Solution() {
+
+    return <section className="section text-center px-24 py-16">
+        <h1 className="text-8xl font-bold">Why Aviatour</h1>
+
+        <div className={'grid grid-cols-3 items-center bg-line mt-8'}>
+            <div className={'col-span-1 py-16 flex items-center'}>
+                <img src={faker.image.urlLoremFlickr({category: 'beach'})} alt={''} className={'rounded-xl aspect-video object-cover'}/>
+            </div>
+            <div className={'mx-auto w-max h-max rounded-full bg-gray-200 p-2'}>
+                <div className={'rounded-full bg-primary p-3'}/>
+            </div>
+            <div className={' col-span-1 py-16 text-start flex flex-col justify-center gap-2'}>
+                <h3 className={'text-5xl font-semibold'}>Best Travel Provider</h3>
+                <p className={'mt-4'}>Your travel memories and stories are our top priority. We ensure that the entire
+                    process of our holiday goes well and is unforgettable</p>
+            </div>
+
+            <div className={' row-span-1 col-span-1 py-16 text-start flex flex-col justify-center gap-2'}>
+                <h3 className={'text-5xl font-semibold'}>Experienced Agent</h3>
+                <p className={'mt-4'}>Our experience and flying hours have proven that the satisfaction of customers who
+                    keep coming back and the various things we have gone through are our basis for providing the best
+                    and always being ready to provide solutions for you.</p>
+            </div>
+            <div className={'mx-auto w-max h-max rounded-full bg-gray-200 p-2'}>
+                <div className={'rounded-full bg-primary p-3'}/>
+            </div>
+            <div className={'row-span-1 col-span-1 py-16 flex items-center'}>
+                <img src={faker.image.urlLoremFlickr({category:'hotel'})} alt={''} className={'rounded-xl aspect-video object-cover'}/>
+            </div>
+
+            <div className={'row-span-1 col-span-1 py-16 flex items-center'}>
+                <img src={faker.image.urlLoremFlickr({category:'pool'})} alt={''} className={'rounded-xl aspect-video object-cover'}/>
+            </div>
+            <div className={'mx-auto w-max h-max rounded-full bg-gray-200 p-2'}>
+                <div className={'rounded-full bg-primary p-3'}/>
+            </div>
+            <div className={' row-span-1 col-span-1 py-16 text-start flex flex-col justify-center gap-2'}>
+                <h3 className={'text-5xl font-semibold'}>Best And Personal Service</h3>
+                <p className={'mt-4'}>We know that your interests are more important than any program we can offer.
+                    Providing the best service for each individual is our principle in perfecting your trip</p>
+            </div>
+            <div className={' row-span-1 col-span-1 py-16 text-start flex flex-col justify-center gap-2'}>
+                <h3 className={'text-5xl font-semibold'}>Free From Problems</h3>
+                <p className={'mt-4'}>
+                    Through comprehensive service in all aspects, you don't need to take care of many things and make
+                    your holiday like work. We will do it for you so you can enjoy every moment without any problems</p>
+            </div>
+            <div className={'mx-auto w-max h-max rounded-full bg-gray-200 p-2'}>
+                <div className={'rounded-full bg-primary p-3'}/>
+            </div>
+            <div className={'row-span-1 col-span-1 py-16 flex items-center'}>
+                <img src={faker.image.urlLoremFlickr({category: 'caribbean'})} alt={''} className={'rounded-xl aspect-video object-cover'}/>
+            </div>
+        </div>
+    </section>
+}
